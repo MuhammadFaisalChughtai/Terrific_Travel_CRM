b948644320796c0e94d42294fe33f6b5e7f5287f
MuhammadFaisalChughtai <muhammadfaisalchughtai@gmail.com>
2026-07-15 13:50:57 +0100

    cr

--- fix_sql.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let sql = fs.readFileSync('e:/Terrific-Travel_TMS/surgical_fix.sql', 'utf8');
-// Fix stray ) before WHERE in UPDATE statements (both id and bookingReference variants)
-sql = sql.replace(/\) WHERE "bookingReference"/g, ' WHERE "bookingReference"');
-sql = sql.replace(/\) WHERE "id"/g, ' WHERE "id"');
-fs.writeFileSync('e:/Terrific-Travel_TMS/surgical_fix.sql', sql);
-console.log('Fixed!');
